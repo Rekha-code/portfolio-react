6b4d16711d72b9c2fa77b436f1ef91675b86b70a
rekha_gorde <rekha.gorde8@gmail.com>
2024-04-07 23:57:13 +0530

    portfolio-react

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       </div>
       <div className="about-sections">
         <div className="about-left">
-          <img src={heroimg} alt="" className="heroimg" />
+          <img src={heroimg} alt="" />
         </div>
         <div className="about-right">
           <div className="about-para">
